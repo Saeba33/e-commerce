6544e2c3a295678a032d94a2bbee421bcc055462
Charles Proust <konoha33@hotmail.fr>
2025-08-06 15:33:39 +0200

    create order controller and twig

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Order;
+use App\Form\OrderFormType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class OrderController extends AbstractController
 {
     #[Route('/order', name: 'app_order')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+
+        $order = new Order();
+        $form = $this->createForm(OrderFormType::class, $order);
+        $form-> handleRequest($request);
+
+
         return $this->render('order/index.html.twig', [
             'controller_name' => 'OrderController',
+            'form' => $form->createView()
         ]);
     }
 }
---
+++ src/Form/OrderFormType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\City;
+use App\Entity\Order;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class OrderFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname')
+            ->add('lastname')
+            ->add('phone')
+            ->add('address')
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('city', EntityType::class, [
+                'class' => City::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Order::class,
+        ]);
+    }
+}
