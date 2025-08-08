d7ce5354fd14b1afcc73537ab00608507770e8e5
Charles Proust <konoha33@hotmail.fr>
2025-08-08 10:04:40 +0200

    edit service cart

--- src/Service/Cart.php
+++ src/Service/Cart.php
@@ -3,8 +3,6 @@
 namespace App\Service;
 
 use App\Repository\ProductRepository;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class Cart
 {
