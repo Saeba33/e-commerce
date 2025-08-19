684d8c2a14ceaec15079acbe9585e7cd2e9fdb60
Charles Proust <konoha33@hotmail.fr>
2025-08-19 14:01:17 +0200

    edit stripepayment to include shipping cost

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -7,21 +7,23 @@ use App\Entity\Order;
 use App\Service\Cart;
 use App\Form\OrderFormType;
 use App\Entity\OrderProducts;
+use App\Service\StripePayment;
 use Symfony\Component\Mime\Email;
 use App\Repository\OrderRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Mailer\MailerInterface;
 
 final class OrderController extends AbstractController
 {
+    public function __construct(private MailerInterface $mailer)
+    {
+    }
 
-    public function __construct(private MailerInterface $mailer) {}
-    
     #region CREATE
     #[Route('/order', name: 'app_order')]
     public function index(Request $request, SessionInterface $session, EntityManagerInterface $entityManager, Cart $cart): Response
@@ -51,25 +53,30 @@ final class OrderController extends AbstractController
 
             if ($order->isPayOnDelivery()) {
 
+                $session->set('cart', []);
+
+                $html = $this->renderView('mail/orderConfirm.html.twig', [
+                    'order' => $order
+
+                ]);
+                $email = (new Email())
+                ->from('maboutique@contact.com')
+                ->to($order->getEmail())
+                ->subject('Confirmation de réception de commande')
+                ->html($html);
+                $this->mailer->send($email);
+
+                return $this->redirectToRoute('order_message');
             }
+            $paymentStripe = new StripePayment();
+            $shippingCost = $order->getCity()->getShippingCost();
+            $paymentStripe->startPayment($cartData, $shippingCost, $order->getId());
+            $stripeRedirectUrl = $paymentStripe->getStripeRedirectUrl();
 
-
-            $session->set('cart', []);
-            
-            $html = $this->renderView('mail/orderConfirm.html.twig', [
-                'order'=> $order
-            ]);
-            $email = (new Email())
-            ->from('maboutique@contact.com')
-            ->to($order->getEmail())
-            ->subject('Confirmation de commande')
-            ->html($html);
-            $this->mailer->send($email);
-
-
-
-            return $this->redirectToRoute('order_message');
+            return $this->redirect($stripeRedirectUrl);
         }
+
+
 
         return $this->render('order/index.html.twig', [
             'form' => $form,
---
+++ src/Service/StripePayment.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Service;
+
+use Stripe\Stripe;
+use Stripe\Checkout\Session;
+
+class StripePayment
+{
+    private $redirectUrl;
+
+    public function __construct()
+    {
+        Stripe::setApiKey($_SERVER['STRIPE_SECRET_KEY']);
+        Stripe::setApiVersion('2025-07-30.basil');
+    }
+
+    public function startPayment($cart, $shippingCost)
+    {
+        $cartProducts = $cart['cart'];
+
+        $products = [
+            [
+                'quantity' => 1,
+                'price' => $shippingCost,
+                'name' => "Frais de livraison"
+            ]
+        ];
+
+        foreach ($cartProducts as $value) {
+            $productItem = [];
+            $productItem['name'] = $value['product']->getName();
+            $productItem['price'] = $value['product']->getPrice();
+            $productItem['quantity'] = $value['quantity'];
+            $products[] = $productItem;
+        }
+
+        $session = Session::create([
+            'line_items' => array_map(fn (array $product) => [
+                'quantity' => (int) $product['quantity'],
+                'price_data' => [
+                    'currency' => 'eur',
+                    'product_data' => [
+                        'name' => $product['name']
+                    ],
+                    'unit_amount' => (int) round($product['price'] * 100),
+                ],
+            ], $products),
+            'mode' => 'payment',
+            'cancel_url' => 'http://127.0.0.1:8000/pay/cancel',
+            'success_url' => 'http://127.0.0.1:8000/pay/success',
+            'billing_address_collection' => 'required',
+            'shipping_address_collection' => [
+                'allowed_countries' => ['FR', 'GB'],
+            ],
+            'metadata' => []
+        ]);
+
+        $this->redirectUrl = $session->url;
+    }
+
+    public function getStripeRedirectUrl()
+    {
+        return $this->redirectUrl;
+    }
+}
